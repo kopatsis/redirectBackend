f9f87fb46c6942ee76d4e035aa4d54c9a69e135f
James Kopatsis <jkopatsis@gmail.com>
2024-08-18 22:24:41 -0700

    more complete cleaning

--- entries/cleanentries.go
+++ entries/cleanentries.go
@@ -9,8 +9,27 @@ import (
 )
 
 func DeleteArchivedEntries(db *gorm.DB) {
-	err := db.Where("archived = ? AND archived_date < ?", true, time.Now().Add(-8*time.Hour)).Delete(&datatypes.Entry{}).Error
+	var entryIDs []int
+
+	err := db.Model(&datatypes.Entry{}).
+		Where("archived = ? AND archived_date < ?", true, time.Now().Add(-8*time.Hour)).
+		Pluck("id", &entryIDs).
+		Error
+
 	if err != nil {
-		log.Printf("Error deleting archived entries: %v", err)
+		log.Printf("Error finding archived entries to delete: %v", err)
+		return
+	}
+
+	if len(entryIDs) > 0 {
+		err = db.Where("id IN ?", entryIDs).Delete(&datatypes.Entry{}).Error
+		if err != nil {
+			log.Printf("Error deleting archived entries: %v", err)
+		}
+
+		err = db.Where("param_key IN ?", entryIDs).Delete(&datatypes.Click{}).Error
+		if err != nil {
+			log.Printf("Error deleting clicks associated with archived entries: %v", err)
+		}
 	}
 }
